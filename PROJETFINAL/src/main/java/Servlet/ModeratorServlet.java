9e9e492bf2251beb6a63abd4ae6a06d9e83406cf
SarahBahhar <sarah.bahhar@etu.cyu.fr>
2023-11-08 22:55:34 +0100

    List Moderator

--- PROJETFINAL/src/main/java/Servlet/ModeratorServlet.java
+++ PROJETFINAL/src/main/java/Servlet/ModeratorServlet.java
@@ -1,4 +1,30 @@
 package Servlet;
 
-public class ModeratorServlet {
+import DAO.ModeratorDAO;
+import Model.Moderateur;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "moderatorServlet", value = "/moderator-servlet")
+public class ModeratorServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ModeratorDAO test = new ModeratorDAO();
+        List<Moderateur> listModerator = test.getListModerateur();
+        request.setAttribute("moderators",listModerator);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/moderatorList.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
---
+++ PROJETFINAL/src/main/webapp/WEB-INF/moderatorList.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="Model.Moderateur" %>
+<%@ page import="java.util.List" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Moderator List</title>
+</head>
+<body>
+<h1>Moderator List</h1>
+<table>
+    <tr>
+        <th>Email</th>
+        <th>Can Add Product</th>
+        <th>Can delete Product</th>
+        <th>Max Product</th>
+    </tr>
+
+    <c:forEach items="${moderators}" var="moderator">
+        <tr>
+            <td>${moderator.email}</td>
+            <td>  ${moderator.peutAjouterProduit}  </td>
+            <td>  ${moderator.peutSupprimerProduit}  </td>
+            <td>  ${moderator.maxProduitsLigne}  </td>
+            <td><button>EDIT</button></td>
+            <td><button>DELETE</button></td>
+    </c:forEach>
+</table>
+<br>
+<br>
+<button>ADD NEW MODERATOR</button>
+</body>
+</html>
