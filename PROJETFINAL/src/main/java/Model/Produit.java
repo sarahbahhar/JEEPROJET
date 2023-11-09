6f734e7ffb98f104ad140a38e56a7e10b4e12531
SarahBahhar <sarah.bahhar@etu.cyu.fr>
2023-11-09 11:36:27 +0100

    Update produit table bdd

--- PROJETFINAL/src/main/java/Model/Produit.java
+++ PROJETFINAL/src/main/java/Model/Produit.java
@@ -1,11 +1,16 @@
 package Model;
 
+import java.math.BigDecimal;
+
 public class Produit {
     private int id;
     private String titre;
     private String description;
     private Integer stock;
     private String email;
+    private String nomImage;
+    private String miniDescription;
+    private BigDecimal prix;
 
     public int getId() {
         return id;
@@ -47,6 +52,30 @@ public class Produit {
         this.email = email;
     }
 
+    public String getNomImage() {
+        return nomImage;
+    }
+
+    public void setNomImage(String nomImage) {
+        this.nomImage = nomImage;
+    }
+
+    public String getMiniDescription() {
+        return miniDescription;
+    }
+
+    public void setMiniDescription(String miniDescription) {
+        this.miniDescription = miniDescription;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -59,6 +88,10 @@ public class Produit {
         if (description != null ? !description.equals(produit.description) : produit.description != null) return false;
         if (stock != null ? !stock.equals(produit.stock) : produit.stock != null) return false;
         if (email != null ? !email.equals(produit.email) : produit.email != null) return false;
+        if (nomImage != null ? !nomImage.equals(produit.nomImage) : produit.nomImage != null) return false;
+        if (miniDescription != null ? !miniDescription.equals(produit.miniDescription) : produit.miniDescription != null)
+            return false;
+        if (prix != null ? !prix.equals(produit.prix) : produit.prix != null) return false;
 
         return true;
     }
@@ -70,6 +103,9 @@ public class Produit {
         result = 31 * result + (description != null ? description.hashCode() : 0);
         result = 31 * result + (stock != null ? stock.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (nomImage != null ? nomImage.hashCode() : 0);
+        result = 31 * result + (miniDescription != null ? miniDescription.hashCode() : 0);
+        result = 31 * result + (prix != null ? prix.hashCode() : 0);
         return result;
     }
 }
