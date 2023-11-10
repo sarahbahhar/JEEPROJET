f299b52423af0132bb2d58e57eee03686c790b63
rensimon1 <rensimon1602@gmail.com>
2023-11-10 15:12:44 +0100

    infoCompte

--- PROJETFINAL/src/main/java/Servlet/infoCompteServlet.java
+++ PROJETFINAL/src/main/java/Servlet/infoCompteServlet.java
@@ -8,23 +8,49 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
+import java.text.SimpleDateFormat;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+
+
+import DAO.InfoCompteDAO;
+import Model.Infocompte;
 
 @WebServlet("/infoCompteServlet")
 public class infoCompteServlet extends HttpServlet {
+    private InfoCompteDAO infocompteDAO;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.sendRedirect("infoCompte.jsp");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //try {
+        try {
             PrintWriter out= response.getWriter();
             HttpSession session = request.getSession();
+
             String email=(String) session.getAttribute("email");
+            String dateString= request.getParameter("date");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = dateFormat.parse(dateString);
+            String telephone=request.getParameter("telephone");
+            String adresse=request.getParameter("adresse");
+            String ville=request.getParameter("ville");
+            Integer codePostal= Integer.parseInt(request.getParameter("codePostal"));
+            String pays=request.getParameter("pays");
             out.println(email);
             out.close();
+            Infocompte infocompte=new Infocompte();
+            //infocompte.setDateNaissance(date);
+            infocompte.setTelephone(telephone);
+            infocompte.setAdresse(adresse);
+            infocompte.setVille(ville);
+            infocompte.setCodePostal(codePostal);
+            infocompte.setPays(pays);
+            infocompteDAO.addInfoCompte(infocompte);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/Vue/login-success.jsp");
+            dispatcher.forward(request, response);
+
             /*if(signUpDAO.isUniqueEmail(email)){
                 Compte c=new Compte(email,password);
                 signUpDAO.addCompte(c);
@@ -35,14 +61,14 @@ public class infoCompteServlet extends HttpServlet {
             }
             else{
                 request.setAttribute("error", "Cette adresse e-mail est déjà utilisée. Veuillez en choisir une autre.");
-            }
+            }*/
 
 
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }*/
-    //}
+        }
+    }
 }
-}
+
