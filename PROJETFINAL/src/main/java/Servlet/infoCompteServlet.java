7acdb8a75a4373c564f70c1fc02f715972ec9905
rensimon1 <rensimon1602@gmail.com>
2023-11-10 10:11:59 +0100

    addInfoCompte

---
+++ PROJETFINAL/src/main/java/Servlet/infoCompteServlet.java
@@ -0,0 +1,48 @@
+package Servlet;
+
+import Model.Compte;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/infoCompteServlet")
+public class infoCompteServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("infoCompte.jsp");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //try {
+            PrintWriter out= response.getWriter();
+            HttpSession session = request.getSession();
+            String email=(String) session.getAttribute("email");
+            out.println(email);
+            out.close();
+            /*if(signUpDAO.isUniqueEmail(email)){
+                Compte c=new Compte(email,password);
+                signUpDAO.addCompte(c);
+
+                session.setAttribute("email", email);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/Vue/login-success.jsp");
+                dispatcher.forward(request, response);
+            }
+            else{
+                request.setAttribute("error", "Cette adresse e-mail est déjà utilisée. Veuillez en choisir une autre.");
+            }
+
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+    //}
+}
+}
