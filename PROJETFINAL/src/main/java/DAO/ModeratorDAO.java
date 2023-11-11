5e9afe3ef7784a873bdca214461fda67e7ee95f3
Abdellah427 <abdellah.hassani2002@gmail.com>
2023-11-11 16:44:07 +0100

    add emailExists

--- PROJETFINAL/src/main/java/DAO/ModeratorDAO.java
+++ PROJETFINAL/src/main/java/DAO/ModeratorDAO.java
@@ -1,8 +1,9 @@
 package DAO;
 
+import Model.Moderateur;
 import org.hibernate.Session;
 import java.util.List;
-import Model.Moderateur;
+import org.hibernate.Transaction;
 
 public class ModeratorDAO {
     public List<Moderateur> getListModerateur() {
@@ -19,5 +20,35 @@ public class ModeratorDAO {
         session.save(m);
         session.getTransaction().commit();
         session.close();
+    }
+
+
+
+
+    public static boolean emailExists(String email) {
+        Transaction transaction = null;
+        Moderateur  moderator = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // Démarrez une transaction
+            transaction = session.beginTransaction();
+            // Obtenez un objet Client en recherchant par e-mail
+            moderator = (Moderateur) session.createQuery("FROM Moderateur C WHERE C.email = :email")
+                    .setParameter("email", email)
+                    .uniqueResult();
+
+            // Si un client est trouvé, l'e-mail existe dans la base de données
+            if (moderator != null) {
+                return true;
+            }
+
+            // Commit la transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                // En cas d'erreur, vous pouvez effectuer un rollback de la transaction ici si nécessaire
+            }
+            e.printStackTrace();
+        }
+        return false;
     }
 }
