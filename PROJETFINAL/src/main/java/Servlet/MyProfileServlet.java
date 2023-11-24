db29891638414902ff23e5ffb9193cc25f3f27a0
rensimon1 <rensimon1602@gmail.com>
2023-11-25 00:49:21 +0100

    avg note reglé

--- PROJETFINAL/src/main/java/Servlet/MyProfileServlet.java
+++ PROJETFINAL/src/main/java/Servlet/MyProfileServlet.java
@@ -1,21 +1,26 @@
 package Servlet;
 
 import DAO.ModeratorDAO;
+import Model.Infocompte;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 
 @WebServlet(name = "MyProfileServlet", value = "/my-profile-servlet")
 public class MyProfileServlet extends HttpServlet {
     private final ModeratorDAO moderatorDAO = new ModeratorDAO();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String emailVendeur = request.getParameter("email"); // Récupération de l'email depuis la requête GET
+        HttpSession session= request.getSession();
+        Infocompte ic=(Infocompte) session.getAttribute("InfoCompte");
+        String emailVendeur = ic.getEmail(); // Récupération de l'email depuis la requête GET
 
         BigDecimal averageRating = moderatorDAO.getAverageRatingByEmail(emailVendeur);
 
