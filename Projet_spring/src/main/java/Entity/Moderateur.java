ea8f93f447b53c3b8694b8b188cef34d4e585f09
rensimon1 <rensimon1602@gmail.com>
2023-11-25 21:11:22 +0100

    Entity

---
+++ Projet_spring/src/main/java/Entity/Moderateur.java
@@ -0,0 +1,100 @@
+package Entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "moderateur", schema = "projet_jee")
+public class Moderateur {
+
+    @Id
+    private String email;
+
+    @Column(name = "peut_ajouter_produit")
+    private byte peutAjouterProduit;
+
+    @Column(name = "peut_supprimer_produit")
+    private byte peutSupprimerProduit;
+
+    @Column(name = "max_produits_ligne")
+    private int maxProduitsLigne;
+
+    @Column(name = "nbBannissement")
+    private int nbBannissement;
+
+    @Column(name = "dateBanni", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateBanni;
+
+    @Column(name = "motifCourtBannissement", length = 100, nullable = false)
+    private String motifCourtBannissement;
+
+    @Column(name = "motifLongBanissement", columnDefinition = "text", nullable = false)
+    private String motifLongBanissement;
+
+    // Getter and Setter methods
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte getPeutAjouterProduit() {
+        return peutAjouterProduit;
+    }
+
+    public void setPeutAjouterProduit(byte peutAjouterProduit) {
+        this.peutAjouterProduit = peutAjouterProduit;
+    }
+
+    public byte getPeutSupprimerProduit() {
+        return peutSupprimerProduit;
+    }
+
+    public void setPeutSupprimerProduit(byte peutSupprimerProduit) {
+        this.peutSupprimerProduit = peutSupprimerProduit;
+    }
+
+    public int getMaxProduitsLigne() {
+        return maxProduitsLigne;
+    }
+
+    public void setMaxProduitsLigne(int maxProduitsLigne) {
+        this.maxProduitsLigne = maxProduitsLigne;
+    }
+
+    public int getNbBannissement() {
+        return nbBannissement;
+    }
+
+    public void setNbBannissement(int nbBannissement) {
+        this.nbBannissement = nbBannissement;
+    }
+
+    public Date getDateBanni() {
+        return dateBanni;
+    }
+
+    public void setDateBanni(Date dateBanni) {
+        this.dateBanni = dateBanni;
+    }
+
+    public String getMotifCourtBannissement() {
+        return motifCourtBannissement;
+    }
+
+    public void setMotifCourtBannissement(String motifCourtBannissement) {
+        this.motifCourtBannissement = motifCourtBannissement;
+    }
+
+    public String getMotifLongBanissement() {
+        return motifLongBanissement;
+    }
+
+    public void setMotifLongBanissement(String motifLongBanissement) {
+        this.motifLongBanissement = motifLongBanissement;
+    }
+}
---
+++ Projet_spring/src/main/java/Entity/Produit.java
@@ -0,0 +1,111 @@
+package Entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produit", schema = "projet_jee")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "titre", length = 255)
+    private String titre;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "nomImage", length = 250)
+    private String nomImage;
+
+    @Column(name = "miniDescription", length = 200)
+    private String miniDescription;
+
+    @Column(name = "prix", precision = 10, scale = 2)
+    private BigDecimal prix;
+
+    @Column(name = "categorie", length = 100, nullable = false)
+    private String categorie;
+
+    // Getter and Setter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomImage() {
+        return nomImage;
+    }
+
+    public void setNomImage(String nomImage) {
+        this.nomImage = nomImage;
+    }
+
+    public String getMiniDescription() {
+        return miniDescription;
+    }
+
+    public void setMiniDescription(String miniDescription) {
+        this.miniDescription = miniDescription;
+    }
+
+    public BigDecimal getPrix() {
+        return prix;
+    }
+
+    public void setPrix(BigDecimal prix) {
+        this.prix = prix;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+}
