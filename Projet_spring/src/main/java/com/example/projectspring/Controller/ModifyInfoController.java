0578b31157fddb691dcf3d410e09e0e3fee7389c
rensimon1 <rensimon1602@gmail.com>
2023-11-28 20:05:08 +0100

    page profil ne se refresh que qd deconnexion

--- Projet_spring/src/main/java/com/example/projectspring/Controller/ModifyInfoController.java
+++ Projet_spring/src/main/java/com/example/projectspring/Controller/ModifyInfoController.java
@@ -2,6 +2,7 @@ package com.example.projectspring.Controller;
 
 import com.example.projectspring.Service.InfoAccountService;
 import com.example.projectspring.Entity.Infocompte;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,15 +28,16 @@ public class ModifyInfoController {
     }
 
     @PostMapping
-    public String modifyInfo(@RequestParam("nom") String nom, @RequestParam("prenom") String prenom, @RequestParam("date") String dateString, @RequestParam("telephone") String telephone, @RequestParam("adresse") String adresse, @RequestParam("ville") String ville, @RequestParam("codePostal") Integer codePostal, @RequestParam("pays") String pays, Model model) {
+    public String modifyInfo(@RequestParam("nom") String nom, @RequestParam("prenom") String prenom, @RequestParam("date") String dateString, @RequestParam("telephone") String telephone, @RequestParam("adresse") String adresse, @RequestParam("ville") String ville, @RequestParam("codePostal") Integer codePostal, @RequestParam("pays") String pays, Model model, HttpSession session) {
         try {
             // Parse la date au format java.sql.Date
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             Date utilDate = dateFormat.parse(dateString);
             java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
 
-            // Récupère l'email à partir de la session (vous devrez peut-être ajuster cette partie en fonction de votre logique de session)
-            String email = "example@example.com"; // Exemple, remplacez par la logique de récupération de l'email
+
+            Infocompte ic =(Infocompte) session.getAttribute("InfoCompte");
+            String email= ic.getEmail();
 
             Infocompte updated = new Infocompte();
             updated.setEmail(email);
